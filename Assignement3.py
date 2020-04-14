9c204b597a58421a2ea8a10b0da489726d191485
theSaab <saba.fath@emailgmail.info>
2020-04-14 16:07:43 -0400

    board making program, not even usefulk, sad

---
+++ Assignement3.py
@@ -0,0 +1,43 @@
+def create_board():
+
+    x = int(input('Give length: '))
+    y = int(input('Give height: '))
+    lst = []
+    for i in range(x):
+        lst.append(' ')
+    for i in range(y):
+        print(lst)
+  
+
+
+
+def display(x, y):
+     
+    for i in range(x+1):
+        if i == x:
+            print('  ' + str(i))
+        else:
+            print('  ' + str(i), end='   ')
+    
+    left =  '+-'
+    right = '--+'
+    middle = '-----'
+    line = ''
+    seperators = '     |'
+    seperation = '|    |'
+
+    for i in range(x+1):
+        line += middle
+    for i in range(x):
+        seperation += seperators
+    
+    for i in range(y):
+        print(left + line + right)
+        print(seperation)
+    print(left + line + right)
+  #  for i in range(y):
+      #  print('|   |', end=' ')
+    #for i in range(y):
+     #   print('|    |    |    |    | \n+-------------------+')
+
+display(4,2)
